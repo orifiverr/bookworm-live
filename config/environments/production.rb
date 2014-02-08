719e33f29cd5225dfa5c9d2870ae61f1f00b2bc0
Ori Shmolovsky <ori.shmolovsky@fiverr.com>
2014-02-08 12:06:51 +0200

    added configurations

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,7 +72,7 @@ BookwormLive::Application.configure do
   }
 
 
-  config.action_mailer.default_url_options = { :host => 'example.com' }
+  config.action_mailer.default_url_options = { :host => 'ori.shmolovsky@gmail.com' }
   # ActionMailer Config
   # Setup for production - deliveries, no errors raised
   config.action_mailer.delivery_method = :smtp
