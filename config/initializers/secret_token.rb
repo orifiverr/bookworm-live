d9a9c0e3420ff9475ef2e426a01b4048a26b4ff8
Ori Shmolovsky <ori.shmolovsky@fiverr.com>
2014-02-08 11:35:53 +0200

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BookwormLive::Application.config.secret_token = '97d4a6e4a75e6fed690badef4a641ab2ae2d3d9def9b4980154b08203c3311056dcd46746319f98c6b8aabc11620ecaa9954daf679b39cc3f48937068a17dd42'
